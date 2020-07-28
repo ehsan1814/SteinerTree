eaa7b796433e3c28672d0d8b58d14d878bea5046
ehsan1814 <mr.ehsan.kaji@gmail.com>
2020-07-28 15:38:10 +0430

    Function add edge added

--- src/MST.py
+++ src/MST.py
@@ -1,2 +1,7 @@
-if __name__ == "__main__":
-    print("ehsan")
+class Graph:
+    def __init__(self , vertices):
+        self.V = vertices   #Number of vertices
+        self.graph = []     #List for store graph
+    
+    def addEdge(self , u , v , wieght):
+        self.graph.append([u,v,wieght])
